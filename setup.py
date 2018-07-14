ca9f8b299acd523da09d5979cd93591258665623
ywaby <ywaby@debian.ywaby@163.com>
2018-07-14 11:10:19 +0800

    format setup

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ install_requires = []
 
 long_description = """
 Convert keil program to sdcc
-support c51 to sdcc
+feature
+- c51 to sdcc
 
-`website/docs <http://github.org>`_
+`website/docs <https://github.com/ywaby/keil2sdcc>`_
 """
 
 setup(name='keil2sdcc',
@@ -15,16 +16,14 @@ setup(name='keil2sdcc',
       license='MIT',
       author='ywaby',
       author_email='ywaby@163.com',
-      url='http://github.com',
+      url='https://github.com/ywaby/keil2sdcc',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Console',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3.6',
           'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'Topic :: Software Development :: task',
-'Topic :: Software Development :: project',
+          'Topic :: Software Development :: Code Generators',
       ],
       keywords="Convert c51 to sdcc",
 
